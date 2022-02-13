3c1869ce717711df3f5815f6937e4bfbfd91bcd9
saretotracy <tracysareto@gmail.com>
2022-02-13 23:15:49 +0300

    add new search-git component

---
+++ src/app/search-git/search-git.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-git',
+  templateUrl: './search-git.component.html',
+  styleUrls: ['./search-git.component.css']
+})
+export class SearchGitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
