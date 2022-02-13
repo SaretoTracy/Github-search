d2632923fa39b6e1e1aaef3df120eedac336cea7
saretotracy <tracysareto@gmail.com>
2022-02-14 01:57:53 +0300

    edit environment

--- src/app/repo-git.service.ts
+++ src/app/repo-git.service.ts
@@ -9,4 +9,45 @@ import { environment } from "./../environments/environment";
   providedIn: 'root'
 })
 export class RepoGitService {
- 
+  repo: any;
+  repoArr: any[] = [];
+  constructor(private http: HttpClient, private router: Router) {}
+  getRepoDetails(user: string) {
+    let promise = new Promise((resolve, reject) => {
+      this.http
+        .get<Interrepo[]>(
+          `https://api.github.com/users/${user}/repos?access_token:${environment.personal_AccessToken}`
+        )
+        .toPromise()
+        .then(
+          (response) => {
+            response.forEach((response) => {
+              this.repo = new Repositories(
+                response.name,
+                response.html_url,
+                new Date(response.created_at),
+                response.clone_url,
+                response.forks_count,
+                response.homepage,
+                response.description,
+                response.stargazers_count
+              );
+              if (response.name !== "") {
+                this.repoArr.push(this.repo);
+              }
+            });
+
+  constructor() { }
+            resolve(response);
+          },
+          (error) => {
+            if ((error.status = 404)) {
+              this.router.navigate(["/err"]);
+            }
+            reject(error);
+          }
+        );
+    });
+    return promise;
+  }
+}
