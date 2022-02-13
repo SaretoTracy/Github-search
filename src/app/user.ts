1b42740d8ab554261fa9e0a8f5eb65c18df33508
saretotracy <tracysareto@gmail.com>
2022-02-14 01:50:10 +0300

    add git-serch service funtionality

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,15 +1,17 @@
-export class User {
-    constructor(
-        public name: string,
-        public userName: string,
-        public bio: string,
-        public avatarUrl: string,
-        public followers: number,
-        public following: number,
-        public star: number,
-        public location: string,
-        public socialMedia: string,
-        public repositories: number,
-        public repo_url: string
-      ) {}
+
+    export class User {
+        constructor(
+          public name: string,
+          public userName: string,
+          public bio: string,
+          public avatarUrl: string,
+          public followers: number,
+          public following: number,
+          public star: number,
+          public location: string,
+          public socialMedia: string,
+          public repositories: number,
+          public repo_url: string
+        ) {}
+      
 }
