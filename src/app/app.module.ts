3f0306a854bd458f5b33ef976b8925376bbef607
saretotracy <tracysareto@gmail.com>
2022-02-14 00:37:14 +0300

    add http module and forms module to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { SearchGitComponent } from './search-git/search-git.component';
 import { RepoComponent } from './repo/repo.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { NavComponent } from './nav/nav.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
