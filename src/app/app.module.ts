8fa33d560b12bda8ae3c4c20de54f0617f968d9b
saretotracy <tracysareto@gmail.com>
2022-02-13 23:27:31 +0300

    add new repo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchGitComponent } from './search-git/search-git.component';
+import { RepoComponent } from './repo/repo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchGitComponent
+    SearchGitComponent,
+    RepoComponent
   ],
   imports: [
     BrowserModule,
