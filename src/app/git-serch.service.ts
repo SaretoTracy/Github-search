91fe1fac85fcb5ed2acdcc7e71ede14189a5ffd5
saretotracy <tracysareto@gmail.com>
2022-02-14 01:18:53 +0300

    link other components to git-serch component

--- src/app/git-serch.service.ts
+++ src/app/git-serch.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Router } from "@angular/router";
 import { HttpClient } from "@angular/common/http";
+import { Interrepo } from './interface/repointerface';
+import { Interuser } from './interface/userinterface';
+import { User } from './user';
+import { Repositories } from './repositories';
 
 @Injectable({
   providedIn: 'root'
