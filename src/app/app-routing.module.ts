687f1880d0f1f4525804821c4f7cc1fa465680ea
saretotracy <tracysareto@gmail.com>
2022-02-14 20:43:48 +0300

    add redirection path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { SearchGitComponent } from './search-git/search-git.component';
 import { RepoComponent } from './repo/repo.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 
+
 const routes: Routes = [ 
   {
     path:"Home", component: SearchGitComponent
@@ -12,6 +13,7 @@ const routes: Routes = [
     path:"repo", component: RepoComponent
   },
   { path:'**', component:NotfoundComponent},
+  { path: '', redirectTo:"/Home", pathMatch:"full"},
  ];
 
 @NgModule({
