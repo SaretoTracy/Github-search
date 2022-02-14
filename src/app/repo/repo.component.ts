2c23f12d4de5dfc8e62c9c5c0ba250db09e0e0aa
saretotracy <tracysareto@gmail.com>
2022-02-14 03:17:19 +0300

    add repo component functionality

--- src/app/repo/repo.component.ts
+++ src/app/repo/repo.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { User } from '../user';
+import { Repositories } from "../repositories";
+import { HttpClient } from "@angular/common/http";
+import { Interrepo } from '../interface/repointerface';
+import { Interuser } from '../interface/userinterface';
+import { GitSerchService } from '../git-serch.service';
+import { RepoGitService } from '../repo-git.service';
 
 @Component({
   selector: 'app-repo',
@@ -6,10 +14,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repo.component.css']
 })
 export class RepoComponent implements OnInit {
-
-  constructor() { }
+  userProfile!: User;
+  rep: any[] = [];
+  constructor(
+    private gitService: GitSerchService,
+    private gitRepoService: RepoGitService
+  ) {}
 
   ngOnInit(): void {
+    this.userProfile = this.gitService.userProfile;
+    this.rep = this.gitRepoService.repoArr;
   }
-
 }
